dae43c9c5be43251d40a5f73eed65224b02d765a
Eliot Alter <eliot@yelp.com>
2017-07-27 11:01:43 -0700

    Remove python2 support.

--- setup.py
+++ setup.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
 from setuptools import find_packages
 from setuptools import setup
 
--- tests/example_test.py
+++ tests/example_test.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
-from __future__ import unicode_literals
-
 from vim_turing_machine import example
 
 
