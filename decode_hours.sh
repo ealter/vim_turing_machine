d9f5a74300a9ae08977e643697b4a6a66576bed4
Ian Fijolek <ifij@yelp.com>
2017-07-28 14:09:12 -0700

    Making it easier to decode hours

---
+++ decode_hours.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+venv/bin/python -m vim_turing_machine.machines.merge_business_hours.decode_hours $@
--- vim_turing_machine/machines/merge_business_hours/decode_hours.py
+++ vim_turing_machine/machines/merge_business_hours/decode_hours.py
@@ -1,6 +1,6 @@
 """Descodes a binary string to a json representation of the business's hours
 after the  merge business hours turing machine has processed it. Reads json from
-stdin and outputs the initial tape."""
+arv1 and outputs the initial tape."""
 import json
 import sys
 
@@ -9,7 +9,7 @@ from vim_turing_machine.constants import BLANK_CHARACTER
 
 def decode_hours(hours, num_bits=5):
     result = []
-    clean_hours = hours.replace(BLANK_CHARACTER, '')
+    clean_hours = hours.replace(BLANK_CHARACTER, '').replace(' ', '')
     index = 0
     while index < len(clean_hours):
         begin = clean_hours[index:index+num_bits]
@@ -21,3 +21,6 @@ def decode_hours(hours, num_bits=5):
         result.append([begin, end])
 
     return result
+
+if __name__ == '__main__':
+    print(json.dumps(decode_hours(sys.argv[1], int(sys.argv[2]))))
