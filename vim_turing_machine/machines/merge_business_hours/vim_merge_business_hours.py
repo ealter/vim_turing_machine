20b450c4cd0ff9c57d894fa263056ff4cd2dbf07
Ian Fijolek <ifij@yelp.com>
2017-07-27 19:51:06 -0700

    Add a vim version of merge business hours

---
+++ vim_turing_machine/machines/merge_business_hours/vim_merge_business_hours.py
@@ -0,0 +1,7 @@
+from vim_turing_machine.machines.merge_business_hours.merge_business_hours import merge_business_hours_transitions
+from vim_turing_machine.vim_machine import VimTuringMachine
+
+
+if __name__ == '__main__':
+    merge_business_hours = VimTuringMachine(merge_business_hours_transitions(), debug=True)
+    merge_business_hours.run(initial_tape=sys.argv[1], max_steps=50)
